441e830d63272716fd5e999d69c027bae6c1bdc4
arthur-cabral <arthurmartins10052002@gmail.com>
2023-10-29 23:47:12 -0300

    chore: Removing database from settings and code from admin

--- backendnutris/backendnutris/settings.py
+++ backendnutris/backendnutris/settings.py
@@ -48,13 +48,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backendnutris.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
